ebe234096aad526c2f0cbb9d5544f2679f90760f
PolymorphicFire <austinbarr121@gmail.com>
2023-08-31 23:40:00 -0700

    part 6 changes

--- Cubethon/Assets/PlayerMovement.cs
+++ Cubethon/Assets/PlayerMovement.cs
@@ -1,31 +1,15 @@
-using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
-
     public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
-    public bool checkForwards = false;
+    public float sidewaysForce = 100f;
     public bool checkLeft = false;
     public bool checkRight = false;
-    public bool checkBackwards = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Debug.Log("Hello, World!");
-        //rb.useGravity = false;
-        //rb.AddForce(0, 200, 500);
-    }
 
     void Update()
     {
-        if (Input.GetKey("w"))
-        {
-            checkForwards = true;
-        }
         if (Input.GetKey("a"))
         {
             checkLeft = true;
@@ -34,34 +18,21 @@ public class PlayerMovement : MonoBehaviour
         {
             checkRight = true;
         }
-        if (Input.GetKey("s"))
-        {
-            checkBackwards = true;
-        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (checkForwards)
-        {
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-            checkForwards = false;
-        }
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
         if (checkLeft)
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
             checkLeft = false;
         }
         if (checkRight)
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
             checkRight = false;
-        }
-        if (checkBackwards)
-        {
-            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
-            checkBackwards = false;
         }
     }
 }
